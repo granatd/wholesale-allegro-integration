b73744028033a4f3b9302a4fe990880802f7651d
Daniel <daniel.granat@op.pl>
2020-03-15 17:26:13 +0100

    Add getting seller.id
    Signed-off-by: Daniel <daniel.granat@op.pl>

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
+import re
 import time
 import pickle
 import logging as log
 import requests
 from pprint import pformat
-from base64 import b64encode
+from base64 import b64encode, b64decode
 
 log.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 log = log.getLogger(__name__)
@@ -118,7 +119,8 @@ def getDeliveryMethods():
 
 
 def getShippingRates():
-    req = requests.Request('GET', 'https://api.allegro.pl/sale/shipping-rates',
+    global tokenObj
+    req = requests.Request('GET', 'https://api.allegro.pl/sale/shipping-rates?seller.id={Seller_IDD}',
                            headers={
                                'authorization': 'Bearer ' + tokenObj['access_token'],
                                'accept': 'application/vnd.allegro.public.v1+json',
@@ -133,9 +135,22 @@ def getShippingRates():
     log.debug(pformat(resp.text))
 
 
+def getSellerID(tokenObj):
+    """Decode sellerID from access token"""
+
+    b64_string = tokenObj['access_token'] + "=" * ((4 - len(tokenObj) % 4) % 4)  # adds proper padding to base64 string
+    matchObj = re.search(b'\"user_name\":\"([0-9]+)\"', b64decode(b64_string))
+    sellerID = matchObj.group(1).decode("UTF-8")
+
+    return sellerID
+
+
 def main():
+    global tokenObj
+
     tokenObj = deviceFlowOAuth()
-    getShippingRates()
+    sellerID = getSellerID(tokenObj)
+    log.debug(sellerID)
 
 
 if __name__ == '__main__':
